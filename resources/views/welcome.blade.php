7a34c0fef62faf3ab4f04875ab50f6ce85e557f6
Airlangga Pasmika <airlangga.pasmika@edi-indonesia.co.id>
2020-11-12 14:40:10 +0700

    Update Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,10 @@
 
     <div class="container-fluid mt--4">
             <div class="row">
-                <div class="col-xl-4 order-xl-1 mt-5 mb-xl-0">
+                <div class="col-md-12 mt-5">
                     <div class="card card-profile">
                         <div class="col-lg-12 text-center">
-                        <img src="{{ asset('argon') }}/img/theme/workflow.jpeg" style="width:1300px; height:auto">
+                        <img src="{{ asset('argon') }}/img/theme/workflow.jpeg" class="img-fluid">
                         </div>
                     </div>
                 </div>
